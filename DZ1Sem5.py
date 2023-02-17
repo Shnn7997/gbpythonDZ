1021827a18f25551400a9e1e25fce9665f96e609
Nikita Shulgin <shnn7997@gmail.com>
2023-02-17 14:46:04 +0700

    Sem5DZ3

---
+++ DZ1Sem5.py
@@ -0,0 +1,17 @@
+# Вычислить число c заданной точностью d
+
+import math
+
+
+def leibniz(d):
+    c = 0
+    i = 0
+    while True:
+        c += (-1) ** i * 4 / (2 * i + 1)
+        if abs(c - math.pi) <= 10 ** (-d):
+            return c
+        i += 1
+
+
+d = float(input('Введите точность d: '))
+print(f'Число c точностью {d} равно {leibniz(d)}')
