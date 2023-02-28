d9cf59f8f80646a0f1dafc24eb1bf6c392b3c96e
Nikita Shulgin <shnn7997@gmail.com>
2023-02-28 12:12:58 +0700

    DZ3Sem7

--- phone.py
+++ phone.py
@@ -1,94 +1,141 @@
-# Задание в группах: Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
-
 import csv
 import json
 
-# приветственное меню
-def welcome_menu():
-    print('Добро пожаловать в телефонный справочник!')
-    print('Выберите действие:')
-    print('1. Вывести список записей')
-    print('2. Добавить запись')
-    print('3. Удалить запись')
-    print('4. Импорт данных из файла')
-    print('5. Экспорт данных в файл')
-    print('6. Выход')
-    choice = int(input('Введите номер действия: '))
-    return choice
-
-# вывод списка записей
-def print_records(records):
-    print('Список записей:')
-    for record in records:
-        print(f'{record[0]}: {record[1]}')
-
-# добавление записи
-def add_record(records):
+# Функция для добавления записи
+def add_contact():
     name = input('Введите имя: ')
-    phone = input('Введите номер телефона: ')
-    records.append([name, phone])
-    print('Запись успешно добавлена!')
+    surname = input('Введите фамилию: ')
+    phone_number = input('Введите номер телефона: ')
 
-# удаление записи
-def remove_record(records):
-    name = input('Введите имя для удаления: ')
-    for record in records:
-        if record[0] == name:
-            records.remove(record)
-            print('Запись успешно удалена!')
+    contact = {
+        'name': name,
+        'surname': surname,
+        'phone_number': phone_number
+    }
+
+    contacts.append(contact)
+    print('Запись добавлена')
+
+
+# Функция для поиска записи
+def find_contact():
+    search_name = input('Введите имя для поиска: ')
+    for contact in contacts:
+        if contact['name'] == search_name:
+            print(f"Имя: {contact['name']}, Фамилия: {contact['surname']}, Номер телефона: {contact['phone_number']}")
             break
     else:
-        print('Запись не найдена!')
+        print('Запись не найдена')
 
-# импорт данных из файла
-def import_records(records):
-    file_name = input('Введите имя файла: ')
-    file_extension = file_name.split('.')[-1]
-    if file_extension == 'csv':
-        with open(file_name, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            for row in reader:
-                records.append(row)
-            print('Данные успешно импортированы!')
-    elif file_extension == 'json':
-        with open(file_name, 'r') as json_file:
-            records.extend(json.load(json_file))
-            print('Данные успешно импортированы!')
-    else:
-        print('Неизвестный формат файла!')
 
-# экспорт данных в файл
-def export_records(records):
-    file_name = input('Введите имя файла: ')
-    file_extension = file_name.split('.')[-1]
-    if file_extension == 'csv':
-        with open(file_name, 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerows(records)
-            print('Данные успешно экспортированы!')
-    elif file_extension == 'json':
-        with open(file_name, 'w') as json_file:
-            json.dump(records, json_file)
-            print('Данные успешно экспортированы!')
-    else:
-        print('Неизвестный формат файла!')
-
-# основной цикл
-def main():
-    records = []
-    while True:
-        choice = welcome_menu()
-        if choice == 1:
-            print_records(records)
-        elif choice == 2:
-            add_record(records)
-        elif choice == 3:
-            remove_record(records)
-        elif choice == 4:
-            import_records(records)
-        elif choice == 5:
-            export_records(records)
-        elif choice == 6:
+# Функция для редактирования записи
+def edit_contact():
+    search_name = input('Введите имя для редактирования: ')
+    for contact in contacts:
+        if contact['name'] == search_name:
+            contact['name'] = input('Введите новое имя: ')
+            contact['surname'] = input('Введите новую фамилию: ')
+            contact['phone_number'] = input('Введите новый номер телефона: ')
+            print('Запись отредактирована')
             break
+    else:
+        print('Запись не найдена')
 
-main()
+
+# Функция для удаления записи
+def delete_contact():
+    search_name = input('Введите имя для удаления: ')
+    for contact in contacts:
+        if contact['name'] == search_name:
+            contacts.remove(contact)
+            print('Запись удалена')
+            break
+    else:
+        print('Запись не найдена')
+
+
+# Функция для экспорта данных в формате json
+def export_json():
+    with open('contacts.json', 'w') as file:
+        json.dump(contacts, file)
+    print('Данные экспортированы в формате json')
+
+
+# Функция для экспорта данных в формате csv
+def export_csv():
+    with open('contacts.csv', 'w') as file:
+        fields = ['name', 'surname', 'phone_number']
+        writer = csv.DictWriter(file, fields, delimiter=';')
+        writer.writeheader()
+        for contact in contacts:
+            writer.writerow(contact)
+    print('Данные экспортированы в формате csv')
+
+
+# Функция для импорта данных из формата json
+def import_json():
+    with open('contacts.json') as file:
+        global contacts
+        contacts = json.load(file)
+    print('Данные импортированы из формата json')
+
+
+# Функция для импорта данных из формата csv
+def import_csv():
+    with open('contacts.csv') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        global contacts
+        contacts = list(reader)
+    print('Данные импортированы из формата csv')
+
+
+# Функция для вывода всех записей
+def show_contacts():
+    for contact in contacts:
+        print(f"Имя: {contact['name']}, Фамилия: {contact['surname']}, Номер телефона: {contact['phone_number']}")
+
+
+# Функция для вывода приветственного меню
+def show_menu():
+    print('1. Добавить запись')
+    print('2. Найти запись')
+    print('3. Редактировать запись')
+    print('4. Удалить запись')
+    print('5. Экспорт данных в формате json')
+    print('6. Экспорт данных в формате csv')
+    print('7. Импорт данных из формата json')
+    print('8. Импорт данных из формата csv')
+    print('9. Показать все записи')
+    print('0. Выход')
+
+
+# Переменная для хранения записей
+contacts = []
+
+# Основной цикл программы
+while True:
+    show_menu()
+    choice = input('Выберите пункт меню: ')
+
+    if choice == '1':
+        add_contact()
+    elif choice == '2':
+        find_contact()
+    elif choice == '3':
+        edit_contact()
+    elif choice == '4':
+        delete_contact()
+    elif choice == '5':
+        export_json()
+    elif choice == '6':
+        export_csv()
+    elif choice == '7':
+        import_json()
+    elif choice == '8':
+        import_csv()
+    elif choice == '9':
+        show_contacts()
+    elif choice == '0':
+        break
+        
+    show_menu()
---
+++ phoneWrong.py
@@ -0,0 +1,94 @@
+# Задание в группах: Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
+
+import csv
+import json
+
+# приветственное меню
+def welcome_menu():
+    print('Добро пожаловать в телефонный справочник!')
+    print('Выберите действие:')
+    print('1. Вывести список записей')
+    print('2. Добавить запись')
+    print('3. Удалить запись')
+    print('4. Импорт данных из файла')
+    print('5. Экспорт данных в файл')
+    print('6. Выход')
+    choice = int(input('Введите номер действия: '))
+    return choice
+
+# вывод списка записей
+def print_records(records):
+    print('Список записей:')
+    for record in records:
+        print(f'{record[0]}: {record[1]}')
+
+# добавление записи
+def add_record(records):
+    name = input('Введите имя: ')
+    phone = input('Введите номер телефона: ')
+    records.append([name, phone])
+    print('Запись успешно добавлена!')
+
+# удаление записи
+def remove_record(records):
+    name = input('Введите имя для удаления: ')
+    for record in records:
+        if record[0] == name:
+            records.remove(record)
+            print('Запись успешно удалена!')
+            break
+    else:
+        print('Запись не найдена!')
+
+# импорт данных из файла
+def import_records(records):
+    file_name = input('Введите имя файла: ')
+    file_extension = file_name.split('.')[-1]
+    if file_extension == 'csv':
+        with open(file_name, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                records.append(row)
+            print('Данные успешно импортированы!')
+    elif file_extension == 'json':
+        with open(file_name, 'r') as json_file:
+            records.extend(json.load(json_file))
+            print('Данные успешно импортированы!')
+    else:
+        print('Неизвестный формат файла!')
+
+# экспорт данных в файл
+def export_records(records):
+    file_name = input('Введите имя файла: ')
+    file_extension = file_name.split('.')[-1]
+    if file_extension == 'csv':
+        with open(file_name, 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(records)
+            print('Данные успешно экспортированы!')
+    elif file_extension == 'json':
+        with open(file_name, 'w') as json_file:
+            json.dump(records, json_file)
+            print('Данные успешно экспортированы!')
+    else:
+        print('Неизвестный формат файла!')
+
+# основной цикл
+def main():
+    records = []
+    while True:
+        choice = welcome_menu()
+        if choice == 1:
+            print_records(records)
+        elif choice == 2:
+            add_record(records)
+        elif choice == 3:
+            remove_record(records)
+        elif choice == 4:
+            import_records(records)
+        elif choice == 5:
+            export_records(records)
+        elif choice == 6:
+            break
+
+main()
